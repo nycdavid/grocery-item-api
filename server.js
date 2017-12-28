ee5c82195db6e7c7901ce44e02a94b34cdaa2eb1
David Ko <david.ko@pvtmethod.com>
2017-12-28 10:27:47 -0500

    Experimentation with async

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const Koa = require('koa');
 const app = new Koa();
 const Port = 1337;
 
+// Logger
+app.use(async (ctx, next) => {
+  
+});
+
 app.use(async ctx => {
   ctx.body = 'Hello World!';
 });
